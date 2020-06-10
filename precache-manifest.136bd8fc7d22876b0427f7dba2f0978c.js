041e147ee9a453122fb4c25b24314be465a862e4
unknown <dejan.obr@hotmail.com>
2020-06-10 15:14:10 +0200

    Updates

---
+++ precache-manifest.136bd8fc7d22876b0427f7dba2f0978c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea629211b0d71ccae19289b924e25174",
+    "url": "/faceRecognitionApp/index.html"
+  },
+  {
+    "revision": "63da9e623aa1e5c72c13",
+    "url": "/faceRecognitionApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "201f398b2be346bf427f",
+    "url": "/faceRecognitionApp/static/css/main.afa3565a.chunk.css"
+  },
+  {
+    "revision": "63da9e623aa1e5c72c13",
+    "url": "/faceRecognitionApp/static/js/2.ee8b2beb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceRecognitionApp/static/js/2.ee8b2beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201f398b2be346bf427f",
+    "url": "/faceRecognitionApp/static/js/main.cc1ab366.chunk.js"
+  },
+  {
+    "revision": "be4b7bde2f9cf6c8544f",
+    "url": "/faceRecognitionApp/static/js/runtime-main.b68c32dc.js"
+  }
+]);
